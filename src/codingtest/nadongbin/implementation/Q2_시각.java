b09e223ec2ab30253e6221cb5de2f9cbae311ab2
swcho <progress0407@gmail.com>
2021-10-27 07:31:54 +0900

    1.인접리스트 구현 2. 시각 풀이2  3. 부품찾기 구현중

--- src/codingtest/nadongbin/implementation/Q2_시각.java
+++ src/codingtest/nadongbin/implementation/Q2_시각.java
@@ -12,19 +12,43 @@ public class Q2_시각 {
     public static final int MAX_MINUTE = 60;
     public static final int MAX_SECOND = 60;
 
+    public static int N = 3;
+
     public static String INPUT_NUM;
     public static int searchCnt = 0;
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        out.print("검색할 숫자를 입력하시오: ");
-
-        INPUT_NUM = String.valueOf(sc.nextInt());
+//        out.print("검색할 숫자를 입력하시오: ");
+//        INPUT_NUM = String.valueOf(sc.nextInt());
 
 //        풀이1();
-        풀이2();
+//        풀이2();
+        풀이3();
+    }
 
+    private static void 풀이3() {
+
+        int cnt = 0;
+        for (int h = 0; h < MAX_HOUR; h++) {
+            for (int m = 0; m < MAX_MINUTE; m++) {
+                for (int s = 0; s < MAX_SECOND; s++) {
+                    if (checkTime(h, m, s)) {
+                        cnt++;
+                    }
+                }
+            }
+        }
+
+        out.println("cnt: " + cnt);
+    }
+
+    private static boolean checkTime(int h, int m, int s) {
+        if (h % 10 == N || m / 10 == N || m % 10 == N || s / 10 == N || s % 10 == N) {
+            return true;
+        }
+        return false;
     }
 
     private static void 풀이2() {
