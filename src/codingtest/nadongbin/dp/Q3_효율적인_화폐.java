bd4d9a8fd99cfe1d6982b7150c7bd36ab166c0cd
swcho <progress0407@gmail.com>
2021-10-29 23:23:30 +0900

    효율적인 화폐 풀이

---
+++ src/codingtest/nadongbin/dp/Q3_효율적인_화폐.java
@@ -0,0 +1,30 @@
+package codingtest.nadongbin.dp;
+
+import java.util.Arrays;
+
+import static java.lang.System.out;
+
+public class Q3_효율적인_화폐 {
+    public static void main(String[] args) {
+        int n = 15;
+        solve(n);
+    }
+
+    private static void solve(int n) {
+        int[] d = new int[n + 1];
+
+        d[2] = 1;
+        d[3] = 1;
+        d[4] = 1;
+
+        for (int i = 5; i <= n; i++) {
+            d[i] = Math.min(d[i - 2] + d[2], d[i - 3] + d[3]);
+        }
+
+        out.println("Arrays.toString(d) = " + Arrays.toString(d));
+
+
+
+    }
+
+}
