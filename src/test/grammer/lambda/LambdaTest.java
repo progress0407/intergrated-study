0f8afb37df1726a7987de0dd04533c6669477770
swcho <progress0407@gmail.com>
2021-12-01 11:50:23 +0900

    람다 공부한 거

--- src/test/grammer/lambda/LambdaTest.java
+++ src/test/grammer/lambda/LambdaTest.java
@@ -1,19 +1,26 @@
 package test.grammer.lambda;
 
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
 public class LambdaTest {
 	public static void main(String[] args) {
-		Foo foo = x -> x + 5;
-		Boo boo = x -> x * 2;
-		foo.doo(10);
+		Function<Integer, Integer> foo = x -> x + 100;
+		Function<Integer, String> boo = String::valueOf;
+		Function<Integer, String> bar = foo.andThen(boo);
+		System.out.println("bar.apply(7) = " + bar.apply(7));
+
+		Supplier<String> supplier = () -> "hello world";
+		System.out.println("supplier = " + supplier.get());
+
+		Consumer<String> consumer = (str) -> System.out.println(str + " " + str + " ");
+		consumer.accept("hello");
 	}
 
 	@FunctionalInterface
-	interface Foo {
-		int doo(int x);
-	}
-
-	@FunctionalInterface
-	interface Boo {
-		int doo(int x);
+	interface FnFoo {
+		Integer call(Integer in);
 	}
 }
