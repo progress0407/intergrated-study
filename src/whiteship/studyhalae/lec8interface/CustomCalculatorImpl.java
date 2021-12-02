0dd89ede967eff1e00352a50fed575a801cc5041
swcho <progress0407@gmail.com>
2021-12-02 14:50:42 +0900

    custom calculator 구현

---
+++ src/whiteship/studyhalae/lec8interface/CustomCalculatorImpl.java
@@ -0,0 +1,7 @@
+package whiteship.studyhalae.lec8interface;
+
+import java.util.function.IntPredicate;
+import java.util.stream.IntStream;
+
+public class CustomCalculatorImpl implements CustomCalculator {
+}
--- src/whiteship/studyhalae/lec8interface/InterfaceMain.java
+++ src/whiteship/studyhalae/lec8interface/InterfaceMain.java
@@ -2,8 +2,20 @@ package whiteship.studyhalae.lec8interface;
 
 public class InterfaceMain {
 	public static void main(String[] args) {
-		test1();
-		test2();
+		// test1();
+		// test2();
+		test3();
+	}
+
+	private static void test3() {
+		CustomCalculatorImpl calculator = new CustomCalculatorImpl();
+		int[] nums = {1, 2, 3, 4};
+		// odd sum = 4
+		// even sum = 6
+		int oddSum = calculator.addOddNumbers(nums);
+		System.out.println("oddSum = " + oddSum);
+		int evenSum = calculator.addEvenNumbers(nums);
+		System.out.println("evenSum = " + evenSum);
 	}
 
 	private static void test2() {
