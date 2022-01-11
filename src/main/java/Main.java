100e92eaff5ea6ff2c4655f7b14edf275007d4c6
swcho <progress0407@gmail.com>
2022-01-12 00:37:31 +0900

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import domain.TestDomain;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("member");
+		TestDomain testDomain = new TestDomain();
+		testDomain.setName("swcho");
+		System.out.println("testDomain = " + testDomain);
+	}
+}
