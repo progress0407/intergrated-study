64ae58379c354beb09063d06435389f4251c8482
progress0407 <progress0407@gmail.com>
2022-06-12 20:33:26 +0900

    merge 예제 작성

--- hibernate-practice/src/main/java/hellojpa/PureJavaMain.java
+++ hibernate-practice/src/main/java/hellojpa/PureJavaMain.java
@@ -1,17 +1,69 @@
 package hellojpa;
 
-import hellojpa.domain.Member;
+import hellojpa.domain.User;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class PureJavaMain {
 
+	EntityManagerFactory emf = Persistence.createEntityManagerFactory("remind-hibernate");
+
+
 	public static void main(String[] args) {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+		PureJavaMain main = new PureJavaMain();
+		main.run();
+	}
+
+	public void run() {
+
+		User user = createMember("user-1", "최초 회원");
+
+		user.setUserName("변경된 회원");
+
+		mergeUser(user);
+
+	}
+
+	private User createMember(String id, String name) {
+
 		EntityManager em = emf.createEntityManager();
 
-		Member member = new Member("swcho");
-		em.persist(member);
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+
+		User user = new User(id, name);
+
+		em.persist(user);
+
+		tx.commit();
+
+		em.detach(user);
+
+		em.close();
+
+		return user;
+	}
+
+	private User mergeUser(User user) {
+
+		EntityManager em2 = emf.createEntityManager();
+
+		EntityTransaction tx = em2.getTransaction();
+		tx.begin();
+
+		User mergedUser = em2.merge(user);
+
+		tx.commit();
+
+		System.out.println("user.getUserName() = " + user.getUserName());
+		System.out.println("mergedUser.getUserName() = " + mergedUser.getUserName());
+		System.out.println("em2.contains(user) = " + em2.contains(user));
+		System.out.println("em2.contains(mergedUser) = " + em2.contains(mergedUser));
+
+		em2.close();
+
+		return mergedUser;
 	}
 }
--- hibernate-practice/src/main/java/hellojpa/domain/User.java
+++ hibernate-practice/src/main/java/hellojpa/domain/User.java
@@ -1,28 +1,26 @@
 package hellojpa.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Getter
+@Getter @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Member {
+@AllArgsConstructor
+public class User {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE)
-	private Long id;
+	private String id;
 
-	private String name;
+	private String userName;
 
-	public Member(String name) {
-		this.name = name;
+	public User(String userName) {
+		this.userName = userName;
 	}
 }
