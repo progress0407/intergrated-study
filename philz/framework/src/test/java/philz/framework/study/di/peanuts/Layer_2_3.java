6b9c4de65b5148c64edc811cdd9109061a632d42
progress0407 <progress0407@gmail.com>
2023-01-03 00:55:10 +0900

    프로젝트 구조 변경

--- philz/framework/src/test/java/philz/framework/study/di/peanuts/Layer_2_3.java
+++
@@ -1,7 +0,0 @@
-package philz.framework.study.di.peanuts;
-
-import philz.framework.study.di.anno.MiddlePeanut;
-
-@MiddlePeanut
-public class Layer_2_3 {
-}
--- philz/framework/src/test/java/philz/framework/study/di/peanuts/Layer_3_1.java
+++
@@ -1,8 +0,0 @@
-package philz.framework.study.di.peanuts;
-
-import philz.framework.study.di.anno.ImPeanut;
-import philz.framework.study.di.anno.SmallPeanut;
-
-@SmallPeanut
-public class Layer_3_1 {
-}
