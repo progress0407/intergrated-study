2f8a5f954b94b8e6de21f232e404eeef390a2313
progress0407 <progress0407@gmail.com>
2023-02-28 03:53:36 +0900

    도원님 2강 필터 적용

--- 0. study/1. alone/[MSA] Spring Cloud MicroService/msa-projects/apigateway-service/src/main/kotlin/com/example/apigatewayservice/Main.kt
+++
@@ -1,5 +0,0 @@
-package com.example.apigatewayservice
-
-fun main() {
-    println("hello api gateway")
-}
--- 0. study/1. alone/[MSA] Spring Cloud MicroService/msa-projects/first-service/src/main/kotlin/com/example/firstservice/FirstServiceController.kt
+++ 0. study/1. alone/[MSA] Spring Cloud MicroService/msa-projects/first-service/src/main/kotlin/com/example/firstservice/FirstServiceController.kt
@@ -1,14 +1,27 @@
 package com.example.firstservice
 
+import lombok.extern.slf4j.Slf4j
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/first-service")
+@Slf4j
 class FirstServiceController {
+
+    private val log = LoggerFactory.getLogger(FirstServiceController::class.java)
 
     @GetMapping("/welcome")
     fun welcome() = "Welcome to the First service."
 
+    @GetMapping("/message")
+    fun message(@RequestHeader("first-request") header: String): String {
+
+        log.info(header)
+
+        return "Hello World to the First service."
+    }
 }
