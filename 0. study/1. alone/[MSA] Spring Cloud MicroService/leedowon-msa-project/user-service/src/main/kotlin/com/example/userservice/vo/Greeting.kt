a40385febe93f409c258457053bdb7cc4e8a222e
progress0407 <progress0407@gmail.com>
2023-03-10 23:40:34 +0900

    잔여 업데이트

---
+++ 0. study/1. alone/[MSA] Spring Cloud MicroService/leedowon-msa-project/user-service/src/main/kotlin/com/example/userservice/vo/Greeting.kt
@@ -0,0 +1,10 @@
+package com.example.userservice.vo
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+
+@Component
+class Greeting(
+    @Value("\${greeting.message}")
+    val message: String
+)
