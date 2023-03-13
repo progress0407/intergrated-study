be5a4c05de7169a28ce51c86ac98ff245a38b1fe
progress0407 <progress0407@gmail.com>
2023-03-13 23:52:02 +0900

    코딩애플 React 6강 & Redux

--- 0. study/1. alone/coding-apple-react/blog/src/App.js
+++ 0. study/1. alone/coding-apple-react/blog/src/App.js
@@ -12,7 +12,7 @@ function App() {
   let [글제목, 글제목_변경] = useState(['남자코트 추천', '강남 우동 맛집', '필즈의 개발 사랑 이야기']);
   let [따봉, 따봉_변경] = useState(0);
   let 따봉_변경_이벤트 = () => { 따봉_변경(따봉 + 1) };
-  
+
   function 제목_바꾸기() {
     var newArray = [...글제목];
     debugger;
@@ -22,13 +22,17 @@ function App() {
 
   return (
     <div className="App">
+
       <div className="black-nav">
         <div style={blackHeader} >개발 Blog</div>
       </div>
+
       <img src={logo} />
-      <button onClick={ 제목_바꾸기 }>코튼 성별 변경</button>
+
+      <button onClick={제목_바꾸기}>코튼 성별 변경</button>
+
       <div className="list">
-        <h3> {글제목[2]} <span onClick={ 따봉_변경_이벤트 }>👍</span> {따봉} </h3>
+        <h3> {글제목[2]} <span onClick={따봉_변경_이벤트}>👍</span> {따봉} </h3>
         <p>4월 15일 발행</p>
         <hr />
         <h3> {글제목[1]} </h3>
@@ -38,8 +42,25 @@ function App() {
         <p>2월 17일 발행</p>
         <hr />
       </div>
+
+      <Modal />
+
     </div>
   );
 }
+
+function Modal() {
+  return (
+    <> {/* 만일 최외각을 한번더 감싸고 싶으면 이걸 이용하면 된다 */}
+      <div className='modal'>
+        <h2>제목 {글제목[0]}</h2>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+      <div></div>
+    </>
+  )
+}
+
 
 export default App;
