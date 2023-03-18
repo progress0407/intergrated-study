b2695c5cdc2ffed9bd68fc53a957cc2104e68f08
progress0407 <progress0407@gmail.com>
2023-03-18 23:14:56 +0900

    Case 문 내용 추가

--- 0. study/1. alone/yhkim/orm/query-dsl/src/test/java/study/querydsl/step1_basic/Dsl_12_Case.java
+++ 0. study/1. alone/yhkim/orm/query-dsl/src/test/java/study/querydsl/step1_basic/Dsl_12_Case.java
@@ -1,10 +1,15 @@
 package study.querydsl.step1_basic;
 
 import static java.lang.System.out;
+import static study.querydsl.entity.QMember.member;
 
+import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.CaseBuilder;
 import java.util.List;
+
+import com.querydsl.core.types.dsl.NumberExpression;
 import org.junit.jupiter.api.Test;
+import study.querydsl.entity.QMember;
 import study.querydsl.support.AbstractQuerydslTest;
 
 class Dsl_12_Case extends AbstractQuerydslTest {
@@ -37,6 +42,28 @@ class Dsl_12_Case extends AbstractQuerydslTest {
 
         for (final String data : dataList) {
             out.println("data = " + data);
+        }
+    }
+
+    @Test
+    void 강의_이후_추가된_내용() {
+        NumberExpression<Integer> rankPath = new CaseBuilder()
+                .when(member.age.between(0, 20)).then(2)
+                .when(member.age.between(21, 30)).then(3)
+                .otherwise(3);
+
+        List<Tuple> result = queryFactory
+                .select(member.username, member.age, rankPath)
+                .from(member)
+                .orderBy(rankPath.asc())
+                .fetch();
+
+        for (Tuple tuple : result) {
+            String username = tuple.get(member.username);
+            Integer age = tuple.get(member.age);
+            Integer rank = tuple.get(rankPath);
+
+            out.printf("username = %s, age = %s, rank = %s \n", username, age, rank);
         }
     }
 }
