f56f5a49ea286bd47204d44aa43955c28394fa3d
progress0407 <progress0407@gmail.com>
2023-03-19 00:16:08 +0900

    P6Spy 프리티 포맷터 리팩터링 및 적용

--- 0. study/1. alone/yhkim/orm/query-dsl/src/main/java/study/querydsl/config/PrettySqlFormatter.java
+++ 0. study/1. alone/yhkim/orm/query-dsl/src/main/java/study/querydsl/config/PrettySqlFormatter.java
@@ -1,6 +1,14 @@
 package study.querydsl.config;
 
+import com.p6spy.engine.logging.Category;
 import com.p6spy.engine.spy.appender.MessageFormattingStrategy;
+import org.hibernate.engine.jdbc.internal.FormatStyle;
+
+import java.util.Locale;
+
+import static com.p6spy.engine.logging.Category.STATEMENT;
+import static org.hibernate.engine.jdbc.internal.FormatStyle.BASIC;
+import static org.hibernate.engine.jdbc.internal.FormatStyle.DDL;
 
 /**
  * p6spy
@@ -18,16 +26,16 @@ public class PrettySqlFormatter implements MessageFormattingStrategy {
                                 String sql,
                                 String url) {
 
-        final String formattedSql = formatSql(sql);
-        if (sql == null) return "";
+//        final String formattedSql = formatSql(sql);
+        final String formattedSql = formatSql2(sql, category);
 
         return now + " | took " + elapsed + "ms | " + category + " | connection " + connectionId + LINE_SEPARATOR + formattedSql + ";";
     }
 
-    private static String formatSql(String sql) {
+    private static String formatSql2(String sql) {
 
         if (sql == null || sql.trim().isEmpty()) {
-            return null;
+            return sql;
         }
 
         return sql.trim()
@@ -36,5 +44,35 @@ public class PrettySqlFormatter implements MessageFormattingStrategy {
                 .replace(" where ", LINE_SEPARATOR + "where" + LINE_SEPARATOR + "    ")
                 .replace(" and ", LINE_SEPARATOR + "  and ")
                 .replace(" or ", LINE_SEPARATOR + "   or ");
+    }
+
+    private static String formatSql2(String sql, String category) {
+
+        if (sql == null || sql.trim().isEmpty()) {
+            return sql;
+        }
+
+        if (isStatement(category)) {
+
+            String prefixStatement = sql.trim().toLowerCase(Locale.ROOT);
+
+            if (isDDL(prefixStatement)) {
+                return DDL.getFormatter().format(sql);
+            }
+
+            return BASIC.getFormatter().format(sql);
+        }
+
+        return sql;
+    }
+
+    private static boolean isStatement(String category) {
+        return STATEMENT.getName().equals(category);
+    }
+
+    private static boolean isDDL(String prefixStatement) {
+        return prefixStatement.startsWith("create")
+                || prefixStatement.startsWith("alter")
+                || prefixStatement.startsWith("comment");
     }
 }
