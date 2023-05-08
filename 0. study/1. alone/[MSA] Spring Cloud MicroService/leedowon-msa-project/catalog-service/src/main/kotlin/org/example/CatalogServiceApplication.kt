505807a83a1a57dbe0e1958a203141e84c6c6731
progress0407 <progress0407@gmail.com>
2023-05-09 00:02:59 +0900

    [이도원님 MSA] Catalog Service 생성

---
+++ 0. study/1. alone/[MSA] Spring Cloud MicroService/leedowon-msa-project/catalog-service/src/main/kotlin/org/example/CatalogServiceApplication.kt
@@ -0,0 +1,13 @@
+package org.example
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+
+@SpringBootApplication
+@EnableDiscoveryClient
+class CatalogServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<CatalogServiceApplication>(*args)
+}
