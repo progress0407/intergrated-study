4d5c93ec6ceec05f398c8e328bebbe81e6e0dfad
progress0407 <progress0407@gmail.com>
2023-05-21 21:10:31 +0900

    [이도원님 MSA] [User Service] Spring Boot 3.0에 맞게 AuthenticationFilter, SecurityConfiguration, SecuritySecondaryConfiguration 추가

--- 0. study/1. alone/[MSA] Spring Cloud MicroService/leedowon-msa-project/user-service/src/main/kotlin/com/example/userservice/security/SecurityConfiguration.kt
+++ 0. study/1. alone/[MSA] Spring Cloud MicroService/leedowon-msa-project/user-service/src/main/kotlin/com/example/userservice/security/SecurityConfiguration.kt
@@ -1,31 +1,45 @@
 package com.example.userservice.security
 
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest
+import com.example.userservice.service.UserService
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 
 
 @EnableWebSecurity
 @Configuration
-class SecurityConfiguration {
+class SecurityConfiguration(
+    private val objectMapper: ObjectMapper,
+    private val userService: UserService,
+    private val bCryptPasswordEncoder: BCryptPasswordEncoder) {
 
     companion object {
         private val WHITE_LIST = arrayOf("/users/**", "/**")
     }
 
     @Bean
-    fun filterChain(http: HttpSecurity): SecurityFilterChain =
-        http.csrf().disable()
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+
+        return http.csrf().disable()
             .headers().frameOptions().disable().and()
+            .addFilter(AuthenticationFilter(objectMapper))
             .authorizeHttpRequests {
                 it.requestMatchers(*WHITE_LIST).permitAll()
 //                    .requestMatchers(PathRequest.toH2Console()).permitAll()
             }.build()
+    }
 
-    @Bean
-    fun  passwordEncoder() = BCryptPasswordEncoder()
+    @Throws(Exception::class)
+    fun authenticationManager(auth: AuthenticationManagerBuilder): AuthenticationManager {
+        auth.userDetailsService<UserDetailsService>(userService).passwordEncoder(bCryptPasswordEncoder)
+        return auth.build()
+    }
+
 }
