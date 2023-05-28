846a8ecae6c9a535a68920cdbdfe1a73522df932
progress0407 <progress0407@gmail.com>
2023-05-28 15:03:42 +0900

    [이도원님 MSA] API Gateway Actuator 적용

---
+++ 0. study/1. alone/[MSA] Spring Cloud MicroService/leedowon-msa-project/apigateway-service/src/main/kotlin/com/example/apigatewayservice/config/SpringActuatorConfig.kt
@@ -0,0 +1,13 @@
+package com.example.apigatewayservice.config
+
+import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SpringActuatorConfig {
+
+    @Bean
+    fun httpExchangeRepository(): HttpExchangeRepository = InMemoryHttpExchangeRepository()
+}
