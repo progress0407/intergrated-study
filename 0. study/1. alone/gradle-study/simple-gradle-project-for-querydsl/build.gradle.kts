3718941f5a12502ae24f30eaf7fba1d6422662b4
progress0407 <progress0407@gmail.com>
2023-10-28 23:51:46 +0900

    create querydsl project for learning gradle

---
+++ 0. study/1. alone/gradle-study/simple-gradle-project-for-querydsl/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+	java
+	id("org.springframework.boot") version "2.7.17"
+	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+}
+
+group = "com"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+
+	runtimeOnly("com.h2database:h2")
+
+	// lomnok
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
