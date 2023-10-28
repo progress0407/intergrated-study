33498896a07ae26ed1ad27dd702a807297342081
progress0407 <progress0407@gmail.com>
2023-10-28 23:51:46 +0900

    create querydsl project for learning gradle

---
+++ 0. study/1. alone/gradle-study/simple-gradle-project-for-querydsl/src/main/java/com/philo/PhiloApplication.java
@@ -0,0 +1,13 @@
+package com.philo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhiloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhiloApplication.class, args);
+	}
+
+}
---
+++ 0. study/1. alone/gradle-study/simple-gradle-project-for-querydsl/src/test/java/com/philo/PhiloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.philo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhiloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
