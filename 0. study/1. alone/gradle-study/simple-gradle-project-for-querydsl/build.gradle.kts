272c6e0f021cbb54286868a5f8295bfadbffa7d9
progress0407 <progress0407@gmail.com>
2023-10-30 00:15:54 +0900

    add querydsl settings

--- 0. study/1. alone/gradle-study/simple-gradle-project-for-querydsl/build.gradle.kts
+++ 0. study/1. alone/gradle-study/simple-gradle-project-for-querydsl/build.gradle.kts
@@ -4,17 +4,28 @@ plugins {
 	id("io.spring.dependency-management") version "1.0.15.RELEASE"
 }
 
+//extra["queryDslVersion"] = "5.0.0"
+
+val queryDslVersion: String = "4.4.0"
+
 group = "com"
 version = "0.0.1-SNAPSHOT"
 
 java {
-
 	sourceCompatibility = JavaVersion.VERSION_17
 }
 
 configurations {
 	compileOnly {
 		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+sourceSets {
+	main {
+		java {
+			setSrcDirs(listOf("$projectDir/src/main/java", "$projectDir/build/generated"))
+		}
 	}
 }
 
@@ -28,11 +39,17 @@ dependencies {
 
 	runtimeOnly("com.h2database:h2")
 
-	// lomnok
+	// Lombok
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+	// QueryDSL
+	implementation("com.querydsl:querydsl-jpa:${queryDslVersion}")
+	annotationProcessor("javax.persistence:javax.persistence-api")
+	annotationProcessor("javax.annotation:javax.annotation-api")
+	annotationProcessor("com.querydsl:querydsl-apt:${queryDslVersion}:jpa")
 }
 
 tasks.withType<Test> {
