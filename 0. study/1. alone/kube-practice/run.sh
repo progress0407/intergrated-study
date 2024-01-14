bcde77ba4eb93ce08cbea20dc4929af2d9ce713a
progress0407 <progress0407@gmail.com>
2024-01-15 04:39:14 +0900

    apply namespace, configMap

--- 0. study/1. alone/kube-practice/run.sh
+++ 0. study/1. alone/kube-practice/run.sh
@@ -1,11 +1,20 @@
 #!bin/bash
 
-ka deployment-was.yml
+ka namespace.yml
 
-ka deployment-db.yml
+ka configmap-db.yml && \
+ka configmap-was.yml
+
+ka deploy-was.yml && \
+ka deploy-db.yml
 
 ka service-conn-was-db.yml
-
 ka service-nodeport.yml
 
 curl $(minikube ip):30000/version
+
+# check
+while true;\                                                                                      
+do curl $(minikube ip):30000/version; \
+sleep 1; \
+done;
